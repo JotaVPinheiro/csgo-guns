865ff5f913842f5b09d16a203a7680aa05312a55
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-04-13 15:23:43 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
-const express = require("express");
-
+const express = require("express")
 const routes = express.Router()
+
+const SkinController = require('./controllers/SkinController')
+
+routes
+    .get('/index', SkinController.index)
 
 module.exports = routes
