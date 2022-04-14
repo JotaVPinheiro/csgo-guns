5864aee4ea477b6f10bf31e5ee95ab14c12451b9
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-04-14 20:23:15 -0300

    added filtering of get guns route by name, maxPrice and minPrice

--- src/controllers/GunController.js
+++ src/controllers/GunController.js
@@ -5,13 +5,23 @@ module.exports = {
     async index(req, res, next) {
         try {
             const params = req.query
+            console.log(params)
+            const page = params.page || 1
             const query = knex('guns')
                 .limit(5)
-                .offset((params.page - 1) * 5)
+                .offset((page - 1) * 5)
 
             if(params.id)
-                query.where({ id })
-
+                query.where('id', params.id)
+            else {
+                if(params.name)
+                    query.whereILike('name', `${params.name}%`)
+                if(params.maxPrice)
+                    query.where('price', '<=', params.maxPrice)
+                if(params.minPrice)
+                    query.where('price', '>=', params.minPrice)
+            }
+            
             const results = await query
 
             return res.json(results)
