8b15050e3214e1c146bd299c81e48c9ca9a07c12
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-04-14 16:46:03 -0300

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,6 @@
-// Update with your config settings.
 require('dotenv').config()
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
+
+e.exports = {
 
   development: {
     client: 'pg',
@@ -20,37 +17,5 @@ module.exports = {
       directory: `${__dirname}/src/database/seeds`
     }
   }
-
-  /*staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }*/
 
 };
