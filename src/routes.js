c383666790c935ce6eb0c02e544c77b369712f7d
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-04-18 14:02:59 -0300

    added image sent in create gun route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const routes = express.Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
 const GunController = require('./controllers/GunController')
 
 routes
     .get('/guns', GunController.index)
-    .post('/guns', GunController.create)
+    .post('/guns', multer(multerConfig).single('file'), GunController.create)
 
 module.exports = routes
