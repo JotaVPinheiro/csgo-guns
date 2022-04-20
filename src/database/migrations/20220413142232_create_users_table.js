537efdc29fb8953cb1e423b8542a77655230ef56
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-04-20 11:30:57 -0300

    create users migration

---
+++ src/database/migrations/20220413142232_create_users_table.js
@@ -0,0 +1,15 @@
+exports.up = knex => knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.string('username')
+        .notNullable()
+        .unique()
+    table.string('email')
+        .notNullable()
+        .unique()
+    table.string('hash_password')
+        .notNullable()
+
+    table.timestamps(true, true)
+})
+
+exports.down = knex => knex.schema.dropTable('users')
