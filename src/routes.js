4943422117c6f398c78ee133e1ebae597a2760f8
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-05-11 21:59:21 -0300

    indenting scripts

--- src/routes.js
+++ src/routes.js
@@ -8,20 +8,20 @@ const UserController = require('./controllers/UserController')
 const ReviewController = require('./controllers/ReviewController')
 
 routes
-    // Gun routes
-    .get('/guns', GunController.index)
-    .post('/guns', multer(multerConfig).single('image'), GunController.create)
-    .put('/guns', GunController.update)
-    .delete('/guns', GunController.delete)
-    // User routes
-    .get('/users', UserController.index)
-    .post('/users', UserController.create)
-    .post('/login', UserController.login)
-    .post('/logout', UserController.logout)
-    .get('/auth', UserController.auth)
-    // Review routes
-    .get('/reviews', ReviewController.index)
-    .post('/reviews', ReviewController.create)
-    .delete('/reviews', ReviewController.delete)
+  // Gun routes
+  .get('/guns', GunController.index)
+  .post('/guns', multer(multerConfig).single('image'), GunController.create)
+  .put('/guns', GunController.update)
+  .delete('/guns', GunController.delete)
+  // User routes
+  .get('/users', UserController.index)
+  .post('/users', UserController.create)
+  .post('/login', UserController.login)
+  .post('/logout', UserController.logout)
+  .get('/auth', UserController.auth)
+  // Review routes
+  .get('/reviews', ReviewController.index)
+  .post('/reviews', ReviewController.create)
+  .delete('/reviews', ReviewController.delete)
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -3,16 +3,16 @@ const express = require('express')
 const app = express()
 
 app
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(routes)
-    .use((req, res, next) => {
-        const error = new Error('Not Found!')
-        error.status = 404
-        next(error)
-    })
-    .use((error, req, res, next) => {
-        res.status(error.status || 500)
-        res.json({ error: error.message })
-    })
-    .listen(8080, () => console.log('Server running...'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(routes)
+  .use((req, res, next) => {
+    const error = new Error('Not Found!')
+    error.status = 404
+    next(error)
+  })
+  .use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+  })
+  .listen(8080, () => console.log('Server running...'))
